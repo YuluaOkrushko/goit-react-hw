7f55fde1be57397146cf693d81d63dd0ccbaf315
YuluaOkrushko <yuluadmitrieva94@gmail.com>
2020-08-03 15:42:02 +0300

    hw-1

---
+++ goit-js-react-hw-01/build/precache-manifest.98889772fe21f56cc3a8d49c2e65abb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921598bf901ba50a2e5112d43f24b48a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77e82a9351a7a5e132a4",
+    "url": "/static/css/main.0f19021a.chunk.css"
+  },
+  {
+    "revision": "010fd7f96190dc80562a",
+    "url": "/static/js/2.0f3adcba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f3adcba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e82a9351a7a5e132a4",
+    "url": "/static/js/main.b947de11.chunk.js"
+  },
+  {
+    "revision": "3dc522dec8b7613b5259",
+    "url": "/static/js/runtime-main.27b0fb24.js"
+  }
+]);
