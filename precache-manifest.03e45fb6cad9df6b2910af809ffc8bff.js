2cecf85d25c9d165fed8e708e115640694f5cd34
YuluaOkrushko <yuluadmitrieva94@gmail.com>
2020-08-10 18:24:34 +0300

    Updates

---
+++ precache-manifest.03e45fb6cad9df6b2910af809ffc8bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8714892824b828f68d53d0670c043bae",
+    "url": "/goit-react-hw/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "e74b2264031515c06827",
+    "url": "/goit-react-hw/goit-react-hw-02-feedback/static/css/main.6035b6c1.chunk.css"
+  },
+  {
+    "revision": "36533198d95b8ac7d3ac",
+    "url": "/goit-react-hw/goit-react-hw-02-feedback/static/js/2.803b8989.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw/goit-react-hw-02-feedback/static/js/2.803b8989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74b2264031515c06827",
+    "url": "/goit-react-hw/goit-react-hw-02-feedback/static/js/main.84fbb970.chunk.js"
+  },
+  {
+    "revision": "d83a70d23925e3c54935",
+    "url": "/goit-react-hw/goit-react-hw-02-feedback/static/js/runtime-main.47e3d0ab.js"
+  }
+]);
